0f9fdaa0cadfc85089fe0bc9e456ff878bdd4b26
nay9 <renenoe94@hotmail.com>
2021-01-03 10:31:24 -0500

    push

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import SearchBar from './SearchBar.js';
+import React from "react";
+import SearchBar from "./SearchBar.js";
 
-const App = () => {
-    return (
-    <div className="ui container" >
-        <SearchBar />
-        
-        </div>
-
-
-    )
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
     }
 
+
+
+
+
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,11 +3,21 @@ import React from "react";
 class SearchBar extends React.Component {
   state = {term: '' };
 
+  onFormSubmit = event => {
+event.preventDefault();
+
+this.props.onSubmit(this.state.term);
+
+  }
+
+
+
+
   render() {
     return (
       <div className="ui segment">
         Search
-        <form className="ui form" onSubmit={(e) => e.preventDefault()}>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input type="text" 
